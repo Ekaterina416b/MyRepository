b907cf9f9916f05d5f7ab6fe320745f4317738f2
Ekaterina <karpina.katia@gmail.com>
2025-03-14 17:42:44 +0300

    Update hello_world.cpp

--- MyRepository/hello_world.cpp
+++ MyRepository/hello_world.cpp
@@ -5,7 +5,7 @@ int
 main()
 {
   std::string name;
-  std::cout << "Enter your name: "; // get the user's name
+  std::cout << "Enter your name: "; // get the user's name !!!!!
   std::cin >> name;
   std::cout << "Hello world from " << name << std::endl;
   return 0;
