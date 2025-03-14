21df999f954c422e41b7dc8d40eea67eb776cb89
Ekaterina Karpina <karpina.katia@gmail.com>
2025-03-14 12:23:09 +0300

    Add comments to the code

--- MyRepository/hello_world.cpp
+++ MyRepository/hello_world.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     std::string name;
-    std::cout << "Enter your name: ";
+    std::cout << "Enter your name: ";//get the user's name
     std::cin >> name;
     std::cout << "Hello world from " << name << std::endl;
     return 0;
